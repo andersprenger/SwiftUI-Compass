803bca9223d736ea88c1e984c10ba3d60e25928d
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-09-02 16:12:46 -0300

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Anderson Renan Paniz Sprenger on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compass
+//
+//  Created by Anderson Renan Paniz Sprenger on 02/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
